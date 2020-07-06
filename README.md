c92a5050c8342497b0baa69e1015743dda06c028
yusufbiberoglu <yusuf@rebase.com.tr>
2020-07-07 00:50:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# symfony-5-github-api
+
+
+After download poroject run following commands;
+
+composer install
+
+yarn install
+
+yarn watch
+
+symfony serve -d
+
+You can access any profile by typing your username as below;
+
+https://localhost:8000/symfony or; https://localhost:8000/profile/symfony
+
+Also by changing the default username setting from the src/Controller/GithubController;
+
+https://localhost:8000
---
+++ src/Controller/GithubController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\GitHubApi;
+use GuzzleHttp\Client;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GithubController extends AbstractController
+{
+    /**
+     * @Route("/{username}", name="github", defaults={"username": "yusufbiberoglu"})
+     */
+    public function github($username)
+    {
+        return $this->render('github/index.html.twig', [
+            'username' => $username
+        ]);
+    }
+
+
+    /**
+     * @Route("/profile/{username}", name="profile")
+     */
+    public function profile(GitHubApi $gitHubApi, $username)
+    {
+        $profile = $gitHubApi->getProfile($username);
+
+        return $this->render('github/profile.html.twig', $profile);
+    }
+
+    /**
+     * @Route("/repos/{username}", name="repos")
+     */
+    public function repos(GitHubApi $gitHubApi, $username)
+    {
+        $repos = $gitHubApi->getRepos($username);
+
+        return $this->render('github/repos.html.twig', $repos);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
